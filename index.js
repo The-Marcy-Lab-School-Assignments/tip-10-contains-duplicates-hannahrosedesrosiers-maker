402443450f3d2554dabe095e7bd22051dcbdf123
[hannahrosedesrosiers-maker] <[hannahrosedesrosiers@gmail.com]>
2025-10-17 10:38:09 -0400

    passed all tests

--- index.js
+++ index.js
@@ -12,8 +12,37 @@
  */
 
 const containsDuplicate = (nums) => {
+  const freq = {}
+  //empty object to keep track of frequency
+  for (let num of nums) {
+    if (freq[num]) {
+      // checks if the number is already int the object
+      return true
+      //if it is in the object already its a dupe so true is printed
+    } else {
+      freq[num] = 1
+      // if not it is added to the freq objest with a value of one since its the first time we see it
+    }
+  }
+  return false
+  // no dupes are found so false s printed
   //write your code here
 };
+
+// another wat to do it using sets
+// const dupe = new Set()
+//   // a set is an object that cant store copies(no duplicates)
+//   for (let num of nums) {
+//     if(dupe.has(num)) {
+//       // this checks if the set already has that number
+//       return true
+//     }
+//     dupe.add(num)
+//     //this adds the number to the set if it wasnt already there
+//   }
+//   return false
+//   // no duplicates, every one is different so false
+
 
 // Export the function for testing
 module.exports = { containsDuplicate };
